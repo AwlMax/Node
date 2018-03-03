1d11fd40040e5b655f4751f09f3c454fa418e281
SirAllone <33838259+SirAllone@users.noreply.github.com>
2018-03-03 22:31:41 +0300

    add login test

--- energy_report_tes_test.js
+++ energy_report_tes_test.js
@@ -1,24 +1,32 @@
+// - Проверка страницы "ОТЧЕТЫ ПО УЧЕТУ ЭНЕРГОРЕСУРСОВ"
+Feature('Energy report_test');
 
-// - Авторизация в системе ('#LoginForm_account': 'test', '#LoginForm_password': 'test')
-Feature('Login test');
+// -> Проверка наличия элементов
 
-// Успешная авторизация
-Scenario('Success login', (I) => {
-   I.amOnPage('/users/user/login');
-   I.fillField('#LoginForm_account', 'test');
-   I.fillField('#LoginForm_password', 'test');
-   I.click('.btn');
-   I.seeElement('.sedmax-page-wrap');
-});
-
-// Ошибочная авторизация
-Scenario('Error login', (I) => {
+Scenario('Check exists of link in report list', (I) => {
     I.amOnPage('/users/user/login');
     I.fillField('#LoginForm_account', 'test');
-    I.fillField('#LoginForm_password', 'test1');
+    I.fillField('#LoginForm_password', 'test');
     I.click('.btn');
-    I.seeElement('.error');
+    I.seeElement('.sedmax-page-wrap');
+    I.amOnPage('/sedmax/web/ui/reports/unfold/index');
+    I.seeElement('//table/tbody/tr[1]/td[3]/div/a');
 });
+
+Scenario('Check exists of group in group selector', (I) => {
+    I.amOnPage('/users/user/login');
+    I.fillField('#LoginForm_account', 'test');
+    I.fillField('#LoginForm_password', 'test');
+    I.click('.btn');
+    I.seeElement('.sedmax-page-wrap');
+    I.amOnPage('/sedmax/web/ui/reports/unfold/index');
+    I.seeElement('//th[@title="Группа"]/div/select[1]');
+});
+
+// Добавить еще поиск элементов
+
+//-> Проверка поведения элементов
+//-> проверка переходов по кнопкам или появления всплывающих окон
 
 // проверка отчетов для теста
 Scenario('Test reports', (I) => {
@@ -55,6 +63,21 @@ Scenario('test forms', (I) =>{
     I.click('Подтвердить');
 });
 
+// - Проверка страницы "Настройка отчета" (при нажатии кнопки "Добавить отчет")
+Feature('Report setup');
+
+// -> Проверка наличия элементов
+//     -> При выборе устройства из дерева, происходит ли отображение данных в левой таблице
+//     -> Проверка выбора каналов по кнопке "Выбрать все каналы" (Доступные каналы) и появления их в таблице
+//     -> Проверка кнопки "Переместить выбранные" и появления в правой таблице
+//     -> Проверка действия кнопок сортировки в таблицах
+//     -> Проверка действия кнопки "Удалить выбранное", в том числе, когда отчеты не выбраны
+//     -> Проверка перехода по кнопке "Порядок каналов" на страницу "Порядок каналов"
+// -> Проверка страницы "Порядка каналов" (при нажатии кнопки "Порядок каналов" на странице )
+// -> Проверка наличия элементов
+//     -> Проверка поведения элементов на странице "Порядка каналов"
+
+
 // Наименование в НАСТРОЙКА ОТЧЕТА
 Scenario('test tuning report name', (I) => {
     I.amOnPage('/users/user/login');
@@ -87,31 +110,40 @@ Scenario('test tuning report name', (I) => {
 
 // тестовое оборудование
 Scenario('test equipment', (I) => {
-	I.amOnPage('/users/user/login');
+    I.amOnPage('/users/user/login');
     I.fillField('#LoginForm_account', 'test');
     I.fillField('#LoginForm_password', 'test');
     I.click('.btn');
     I.seeElement('.sedmax-page-wrap');
     I.amOnPage('/sedmax/web/ui/reports/unfold/index');
     I.seeElement("//div[@class='actions']/div/a");
-	I.click("//div[@class='actions']/div/a");
-	
-	I.seeElement("//a[@id=['obj-12_anchor']/");
-	I.click("//a[]");	
-	I.click("//li[@id='obj-12']/i[1]");
+    I.click("//div[@class='actions']/div/a");
+
+    I.seeElement("//a[@id=['obj-12_anchor']/");
+    I.click("//a[]");
+    I.click("//li[@id='obj-12']/i[1]");
     I.click("//li[@id='dev-1006']/i[1]");
 });
 
 // доступные каналы. Выбрать все каналы
 Scenario('test available channels', (I) => {
-	I.amOnPage('/users/user/login');
+    I.amOnPage('/users/user/login');
     I.fillField('#LoginForm_account', 'test');
     I.fillField('#LoginForm_password', 'test');
     I.click('.btn');
     I.seeElement('.sedmax-page-wrap');
     I.amOnPage('/sedmax/web/ui/reports/unfold/index');
     I.seeElement("//div[@class='actions']/div/a");
-	I.click("//div[@class='actions']/div/a");
-	I.click("//div[@class='col-md-4']");
-	I.seeElement("//div[@class='btn blue mr5']");
+    I.click("//div[@class='actions']/div/a");
+    I.click("//div[@class='col-md-4']");
+    I.seeElement("//div[@class='btn blue mr5']");
+});
+
+// ОТЧЕТЫ ПО УЧЕТУ ЭНЕРГОРЕСУРСОВ, кнопка удалить выбранное
+Scenario('test delete selected', (I) => {
+	I.amOnPage('/users/user/login');
+    I.fillField('#LoginForm_account', 'test');
+    I.fillField('#LoginForm_password', 'test');
+    I.click('.btn');
+    I.seeElement('')
 })
--- login_test_test.js
+++ login_test_test.js
@@ -1,11 +1,20 @@
-
+// - Авторизация в системе ('#LoginForm_account': 'test', '#LoginForm_password': 'test')
 Feature('Login test');
 
-Scenario('test something', (I) => {
-// создали первый тест файл
+// Успешная авторизация
+Scenario('Success login', (I) => {
    I.amOnPage('/users/user/login');
    I.fillField('#LoginForm_account', 'test');
    I.fillField('#LoginForm_password', 'test');
    I.click('.btn');
    I.seeElement('.sedmax-page-wrap');
+});
+
+// Ошибочная авторизация
+Scenario('Error login', (I) => {
+    I.amOnPage('/users/user/login');
+    I.fillField('#LoginForm_account', 'test');
+    I.fillField('#LoginForm_password', 'test1');
+    I.click('.btn');
+    I.seeElement('.error');
 });
