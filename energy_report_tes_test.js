2e3d79f6f470a34cec1d8113c4942ba99fa1f252
SirAllone <33838259+SirAllone@users.noreply.github.com>
2018-03-03 20:16:28 +0300

    add test

--- energy_report_tes_test.js
+++ energy_report_tes_test.js
@@ -1,6 +1,7 @@
 
 Feature('Energy report_test');
 
+// проверка отчетов для теста
 Scenario('Test reports', (I) => {
     I.amOnPage('/users/user/login');
     I.fillField('#LoginForm_account', 'test');
@@ -13,6 +14,7 @@ Scenario('Test reports', (I) => {
     I.click('Отмена');
 });
 
+// наименование отчетов для теста
 Scenario('test something', (I) => {
     I.amOnPage('/users/user/login');
     I.fillField('#LoginForm_account', 'test');
@@ -23,6 +25,7 @@ Scenario('test something', (I) => {
     I.seeElement('.react-bs-table-container');
 });
 
+// клики по отчетам для тестов
 Scenario('test forms', (I) =>{
     I.amOnPage('/users/user/login');
     I.fillField('#LoginForm_account', 'test');
@@ -33,6 +36,7 @@ Scenario('test forms', (I) =>{
     I.click('Подтвердить');
 });
 
+// Наименование в НАСТРОЙКА ОТЧЕТА
 Scenario('test tuning report name', (I) => {
     I.amOnPage('/users/user/login');
     I.fillField('#LoginForm_account', 'test');
@@ -46,3 +50,49 @@ Scenario('test tuning report name', (I) => {
     I.fillField("//div[@class='form-body']/div[@class='row']/div[@class='col-md-2']/div[@class='form-group']/input[@class='form-control']", "test");
 });
 
+// тестовое оборудование. Выборка по оборудованию
+Scenario('test tuning report name', (I) => {
+    I.amOnPage('/users/user/login');
+    I.fillField('#LoginForm_account', 'test');
+    I.fillField('#LoginForm_password', 'test');
+    I.click('.btn');
+    // I.seeElement('.sedmax-page-wrap');
+    I.amOnPage('/sedmax/web/ui/reports/unfold/index');
+    I.seeElement("//div[@class='actions']/div/a");
+    I.click("//div[@class='actions']/div/a");
+    I.seeElement("//div[@class='form-body']/div[@class='row']/div[@class='col-md-2']/div[@class='form-group']/input[@class='form-control']");
+    I.fillField("//div[@class='form-body']/div[@class='row']/div[@class='col-md-2']/div[@class='form-group']/input[@class='form-control']", "test");
+    I.click("//li[@id='obj-12']/i[1]");
+    I.click("//li[@id='dev-1006']/i[1]");
+});
+
+// тестовое оборудование
+Scenario('test equipment', (I) => {
+	I.amOnPage('/users/user/login');
+    I.fillField('#LoginForm_account', 'test');
+    I.fillField('#LoginForm_password', 'test');
+    I.click('.btn');
+    I.seeElement('.sedmax-page-wrap');
+    I.amOnPage('/sedmax/web/ui/reports/unfold/index');
+    I.seeElement("//div[@class='actions']/div/a");
+	I.click("//div[@class='actions']/div/a");
+	
+	I.seeElement("//a[@id=['obj-12_anchor']/");
+	I.click("//a[]");	
+	I.click("//li[@id='obj-12']/i[1]");
+    I.click("//li[@id='dev-1006']/i[1]");
+});
+
+// доступные каналы. Выбрать все каналы
+Scenario('test available channels', (I) => {
+	I.amOnPage('/users/user/login');
+    I.fillField('#LoginForm_account', 'test');
+    I.fillField('#LoginForm_password', 'test');
+    I.click('.btn');
+    I.seeElement('.sedmax-page-wrap');
+    I.amOnPage('/sedmax/web/ui/reports/unfold/index');
+    I.seeElement("//div[@class='actions']/div/a");
+	I.click("//div[@class='actions']/div/a");
+	I.click("//div[@class='col-md-4']");
+	I.seeElement("//div[@class='btn blue mr5']");
+})
