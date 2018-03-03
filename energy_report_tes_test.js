a79fff587c0a6076f95d200d09ef51838337fa52
SirAllone <33838259+SirAllone@users.noreply.github.com>
2018-03-03 22:25:59 +0300

    add  test 2

--- energy_report_tes_test.js
+++ energy_report_tes_test.js
@@ -1,5 +1,24 @@
 
-Feature('Energy report_test');
+// - Авторизация в системе ('#LoginForm_account': 'test', '#LoginForm_password': 'test')
+Feature('Login test');
+
+// Успешная авторизация
+Scenario('Success login', (I) => {
+   I.amOnPage('/users/user/login');
+   I.fillField('#LoginForm_account', 'test');
+   I.fillField('#LoginForm_password', 'test');
+   I.click('.btn');
+   I.seeElement('.sedmax-page-wrap');
+});
+
+// Ошибочная авторизация
+Scenario('Error login', (I) => {
+    I.amOnPage('/users/user/login');
+    I.fillField('#LoginForm_account', 'test');
+    I.fillField('#LoginForm_password', 'test1');
+    I.click('.btn');
+    I.seeElement('.error');
+});
 
 // проверка отчетов для теста
 Scenario('Test reports', (I) => {
